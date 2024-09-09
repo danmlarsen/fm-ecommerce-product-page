f2953a5afa892f87114702660dd7b6532146d248
Dan M. Larsen <larsendanmarius@gmail.com>
2024-09-09 14:21:15 +0200

    Display total amount on cart icon

--- ts/Cart.ts
+++ ts/Cart.ts
@@ -10,24 +10,40 @@ interface CartItem {
 export class Cart {
     private items: CartItem[] = [];
 
-    calcTotal(): number {
-        return 0;
+    calcTotalAmount(): number {
+        return this.items.reduce((acc, cur) => acc + cur.amount, 0);
     }
 
     addToCart(item: CartItem): void {
         this.items.push(item);
 
+        this.renderAmount();
         this.renderCart();
     }
 
     removeFromCart(removeIndex: number): void {
         this.items = this.items.filter((_, index) => index !== removeIndex);
 
+        this.renderAmount();
         this.renderCart();
     }
 
     getItems(): CartItem[] {
         return this.items;
+    }
+
+    renderAmount(): void {
+        const cartBtnElement = document.querySelector('.cart-btn__amount')!;
+
+        const totalAmount = this.calcTotalAmount();
+
+        if (totalAmount === 0) {
+            cartBtnElement.classList.remove('cart-btn__amount--show');
+        }
+        else {
+            cartBtnElement.textContent = totalAmount.toString();
+            cartBtnElement.classList.add('cart-btn__amount--show');
+        }
     }
 
     renderCart(): void {
