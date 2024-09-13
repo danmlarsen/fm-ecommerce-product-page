f0d9b54953163c4071123221fcfa10fe5418b418
danmlarsen <larsendanmarius@gmail.com>
2024-09-13 16:32:33 +0200

    Minor adjustments

--- ts/CartForm.ts
+++ ts/CartForm.ts
@@ -83,7 +83,7 @@ export class CartForm {
 
     private render(): void {
         const markup = `
-            <form class="cart-form" action="" method="post" novalidate>
+            <form class="cart-form" action="#" method="post" novalidate>
                 <div class="cart-form__amount">
                     <button class="cart-form__amount-btn cart-form__amount-btn--decrement" aria-label="Decrease items button" type="button">
                         <img src="${minusIcon}" alt="Minus icon" />
@@ -94,12 +94,7 @@ export class CartForm {
                     </button>
                 </div>
                 <button class="btn btn--add-to-cart" aria-label="Add item to cart button">
-                    <svg class="cart-icon" alt="Cart icon" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
-                            fill-rule="nonzero"
-                        />
-                    </svg>
+                    <svg viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill-rule="nonzero"/></svg>
                     Add to cart
                 </button>
             </form>
